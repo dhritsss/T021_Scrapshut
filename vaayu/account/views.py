67a3cd5b76cfc9958fbd0d2152661886d9caf2e3
Shrey Sumariya <you@example.com>
2020-09-26 21:15:45 +0530

    form.py updated

--- vaayu/account/views.py
+++ vaayu/account/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
-
-
+from .forms import CreateUser,NgoUser
+from .models import *
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 
 def RegisterAsUser(request):
+    form = CreateUser()
+    if request.method == 'POST':
+        form = CreateUser(request.post)
+        if form.is_valid():
+            user_form = user(
+                fname = form.cleaned_data['fname'],
+                lname = form.cleaned_data['lname'],
+                email = form.cleaned_data['email'],
+                phone = form.cleaned_data['phone'],
+                password1 = form.cleaned_data['password1'],
+                password2 = form.cleaned_data['password2'],
+                Address = form.cleaned_data['address'],
+                country = form.cleaned_data['country'],
+                pincode = form.cleaned_data['pincode'],
+            )
+
+
+
+
     return render(request,"register-donor.html")
 
 def RegisterAsNgo(request):
