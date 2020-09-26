93d5f94317773baffc5afd183925fea4b00fc296
Shrey Sumariya <you@example.com>
2020-09-26 19:26:17 +0530

    static files handling on process

--- vaayu/account/urls.py
+++ vaayu/account/urls.py
@@ -5,4 +5,8 @@ from . import  views
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('',views.index,name="index"),
+    path('RegisterAsUser',views.RegisterAsUser,name = "RegisterAsUser"),
+    path('RegisterAsNgo',views.RegisterAsNgo,name = "RegisterAsNgo"),
+    path('LoginAsNgo',views.LoginAsNgo,name = "LoginAsNgo"),
+    path('LoginAsUser',views.LoginAsUser,name="LoginAsUser"),
 ]
